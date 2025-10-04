ad83484e8160963f74b6cb4f9f598625deb2176f
abrarabdulrahman179-lab <abrarabdulrahman179@gmail.com>
2025-10-04 15:20:06 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,10 @@
+<!-- الشاشة الرئيسية -->
+<div class="screen active">
+    <h2>مرحبا بك في تطبيق طبيب معك</h2>
+    <div class="home-options">
+        <a href="index.php?screen=booking" class="home-item">حجز مواعيد</a>
+        <a href="index.php?screen=diagnosis" class="home-item">تشخيص المرض</a>
+        <a href="#" class="home-item">الاشعارات</a>
+        <a href="#" class="home-item">المعدادات</a>
+    </div>
+</div>
