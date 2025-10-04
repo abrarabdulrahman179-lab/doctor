ca8496ccc5f4a21cfff2b917e499507e8820df1b
abrarabdulrahman179-lab <abrarabdulrahman179@gmail.com>
2025-10-04 15:19:31 +0300

    Create start.php

---
+++ start.php
@@ -0,0 +1,7 @@
+<!-- شاشة البدء -->
+<div class="screen active">
+    <div class="start-screen">
+        <h1 class="app-title">طبيبي معك</h1>
+        <a href="index.php?screen=home" class="start-button">Start</a>
+    </div>
+</div>
